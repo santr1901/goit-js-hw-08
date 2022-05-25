909ffe2b04ed81eea4e824067a8968132c3ac316
santr1901 <santr199018@gmail.com>
2022-05-25 15:40:56 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+galleryItems.map((item)=>{
+    gallery.insertAdjacentHTML('afterbegin', `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>`);
+  });
+  
+
+  var lightbox = new SimpleLightbox('.gallery a', {captionsData:'alt',captionDelay: '250'});
